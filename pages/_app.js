634e2c8cfff9737ad2d360a29e1ef81198afec86
fir4tozden <firatozden01@gmail.com>
2023-04-19 21:54:43 +0300

    regulations

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import "~/styles/global.css";
 
-export default function __App__({ Component, PageProps }) {
+export default function __App__({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
       </Head>
-      <Component {...PageProps}/>
+      <Component {...pageProps}/>
     </>
   )
 };

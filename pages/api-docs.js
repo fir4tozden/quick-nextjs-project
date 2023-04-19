bb41062b414cf321002b54402988cc8fc80d6f55
fir4tozden <firatozden01@gmail.com>
2023-04-19 04:44:32 +0300

    regulations

---
+++ pages/api-docs.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import Head from "next/head";
+
+let config = {
+  "title": "API documentation",
+  "description": "We explain how the sample API in this project works."
+};
+
+export default function Page() {
+  return (
+    <>
+      <Head>
+        <title>{config.title}</title>
+        <meta name="description" content={config.description}/>
+        <meta property="og:title" content={config.title}/>
+        <meta property="og:description" content={config.description}/>
+        <meta property="og:image" content="/favicon.ico"/>
+        <meta name="robots" content="index, follow"/>
+        <meta name="revisit-after" content="7 days"/>
+      </Head>
+
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </>
+  )
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Head from "next/head"; 
+import Link from "next/link";
 import NextjsLogo from "../components/NextjsLogo.js";
 
 var config = {
